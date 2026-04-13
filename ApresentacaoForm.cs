51d9366e0daba2d8829da483a73e24cc503d0e42
LabInfo <LabInfo@TERREOA2M4>
2026-04-13 12:18:11 -0300

    Adicionar arquivos de projeto.

---
+++ ApresentacaoForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using POO_CSharp_Git;
+
+namespace POO_C__Git
+{
+    internal class ApresentacaoForm: Form
+    {
+        //Criar um objeto label
+        Label titulo1 = new Label();
+        Label titulo2 = new Label();
+        Automovel obj1 = new Automovel();
+
+        // CONSTRUTOR
+        public ApresentacaoForm()
+        {
+            this.Text = " Apresentação ";
+            this.Size = new Size(500,500);
+            //configuração do label titulo1
+            titulo1.Text = "Bem vindo ao inferno";
+            titulo1.Font = new Font("Arial", 20);
+            titulo1.AutoSize = true; //ajustar no tamanho do texto
+            titulo1.Location = new Point(250, 250);
+
+            //adicionar a marca a objeto 
+            obj1.Marca = "Ferrari";
+            titulo2.Text = obj1.Marca;
+
+            //fazer o elemento exibir no from
+            this.Controls.Add(titulo1);
+        }
+    }
+}
---
+++ Automovel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_CSharp_Git
+{
+    internal class Automovel
+    {
+        //visibilidade: private, public, protected, internal
+            //public = pode ser acessado por qualquer classe
+            //private = só pode ser acessado pela própria classe
+            //protected = só acessada pela própria classe e pela classe derivada
+            //internal = só pode ser acessada pelo mesmo assembly
+
+        // Criar umn construtor
+
+        public string Marca { get; set; }
+        public Automovel() { 
+            
+        }
+    }
+}
