827628a2afc98daaae3773be4bd1779f7de43620
DanDani-01 <daniel.rt.oliveira@gmail.com>
2026-04-16 10:48:47 -0300

    Botão

--- ApresentacaoForm.cs
+++ ApresentacaoForm.cs
@@ -6,14 +6,17 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
 using POO_CSharp_Git;
+using System.Drawing.Text;
 
 namespace POO_C__Git
 {
+    //Toda classe tem um construtor, mesmo que não seja declarado
     internal class ApresentacaoForm: Form
     {
-        //Criar um objeto label
+        //Criar um objeto
         Label titulo1 = new Label();
         Label titulo2 = new Label();
+        Button btn1 = new Button();
         Automovel obj1 = new Automovel();
 
         // CONSTRUTOR
@@ -21,22 +24,50 @@ namespace POO_C__Git
         {
             this.Text = " Apresentação ";
             this.Size = new Size(500,500);
+
             //configuração do label titulo1
             titulo1.Text = "Bem vindo ao inferno";
             titulo1.Font = new Font("Arial", 20);
             titulo1.AutoSize = true; //ajustar no tamanho do texto
             titulo1.Location = new Point(250, 250);
+            btn1.Text = "Botão legal";
+            btn1.Location = new Point(50, 50);
+            btn1.Click += Clicar; //evento de clique
 
             //adicionar a marca a objeto 
             obj1.Marca = "Ferrari";
             titulo2.Text = obj1.Marca;
 
             //fazer o elemento exibir no from
-            //fazer um array de controls
-            Control[] elementos = { titulo1, titulo2 };
+                //fazer um array de controls
+            Control[] elementos = { titulo1, titulo2, btn1 };
             this.Controls.AddRange(elementos);
 
-            //this.Controls.Add(titulo1);
+            //Forma única: this.Controls.Add(titulo1);
+        }
+
+        //metodo de evento
+        private void Clicar(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clicou no botão");
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ApresentacaoForm
+            // 
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Name = "ApresentacaoForm";
+            this.Load += new System.EventHandler(this.ApresentacaoForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void ApresentacaoForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
