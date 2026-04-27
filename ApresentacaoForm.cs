b9067c45484bae9af70773c1ae7d5c6d3d9ab00c
DanDani-01 <daniel.rt.oliveira@gmail.com>
2026-04-27 12:27:35 -0300

    Imagen

--- ApresentacaoForm.cs
+++ ApresentacaoForm.cs
@@ -7,18 +7,30 @@ using System.Windows.Forms;
 using System.Drawing;
 using POO_CSharp_Git;
 using System.Drawing.Text;
+using System.Net.Http.Headers;
 
 namespace POO_C__Git
 {
     //Toda classe tem um construtor, mesmo que não seja declarado
     internal class ApresentacaoForm: Form
     {
-        //Criar um objeto
-        Label titulo1 = new Label();
-        Label titulo2 = new Label();
-        Button btn1 = new Button();
-        Automovel obj1 = new Automovel();
+        //criar um objeto do automóvel
+        Automovel carro01 = new Automovel();
+        Automovel modelo01 = new Automovel();
+        Automovel tipo = new Automovel();
+        Automovel velocidade = new Automovel();
+        Automovel ano = new Automovel();
 
+        //Criar um objeto
+        PictureBox img = new PictureBox();
+        Label titulo1 = new Label();
+        Label txtMarca = new Label();
+        Label txtModelo = new Label();
+        Label txtTipo = new Label();
+        Label txtVelocidade = new Label();
+        Label txtAno = new Label();
+        Button btn1 = new Button();
+        
         // CONSTRUTOR
         public ApresentacaoForm()
         {
@@ -34,13 +46,23 @@ namespace POO_C__Git
             btn1.Location = new Point(50, 50);
             btn1.Click += Clicar; //evento de clique
 
-            //adicionar a marca a objeto 
-            obj1.Marca = "Ferrari";
-            titulo2.Text = obj1.Marca;
+
+
+            //fazer apresentação do carr01
+            //carro01.SetMarca("Ferrari");
+            //this.titulo2.Text = "Marca " + carro01.GetMarca();
+            //this.titulo2.Location = new Point(280, 280);
+
+            img.Image = Image.FromFile(@"C:\\Users\\LabInfo\\Source\\Repos\\POO_CSharp\\img\\f40.jpg");
+            img.SizeMode = PictureBoxSizeMode.AutoSize;
+            img.Width = 50;
+            img.Height = 10;
+            this.img.Location = new Point(280, 400);
+
 
             //fazer o elemento exibir no from
-                //fazer um array de controls
-            Control[] elementos = { titulo1, titulo2, btn1 };
+            //fazer um array de controls
+            Control[] elementos = { titulo1, txtAno, txtMarca, txtModelo, txtVelocidade, btn1, img };
             this.Controls.AddRange(elementos);
 
             //Forma única: this.Controls.Add(titulo1);
@@ -50,7 +72,23 @@ namespace POO_C__Git
         private void Clicar(object sender, EventArgs e)
         {
             MessageBox.Show("Clicou no botão");
+            carro01.Marca = "Ferrari";
+            carro01.Ano = 2010;
+            carro01.Modelo = "F40";
+            carro01.Velocidade = "300km/h";
+           
+            txtMarca.Text = "Marca: " + carro01.Marca;
+            txtMarca.Location = new Point(280, 280);
+            txtModelo.Text = "Modelo: " + carro01.Modelo;
+            txtModelo.Location = new Point(280, 310);   
+            txtVelocidade.Text = "Velocidade: " + carro01.Velocidade;
+            txtVelocidade.Location = new Point(280, 340);
+            txtAno.Text = "Ano: " + carro01.Ano;
+            txtAno.Location = new Point(280, 370);  
+
+            
         }
+
 
         private void InitializeComponent()
         {
--- Automovel.cs
+++ Automovel.cs
@@ -9,16 +9,42 @@ namespace POO_CSharp_Git
     internal class Automovel
     {
         //visibilidade: private, public, protected, internal
-            //public = pode ser acessado por qualquer classe
-            //private = só pode ser acessado pela própria classe
-            //protected = só acessada pela própria classe e pela classe derivada
-            //internal = só pode ser acessada pelo mesmo assembly
+        //public = pode ser acessado por qualquer classe
+        //private = só pode ser acessado pela própria classe
+        //protected = só acessada pela própria classe e pela classe derivada
+        //internal = só pode ser acessada pelo mesmo assembly
 
-        // Criar umn construtor
 
+
+        //criar uma propriedade para a marca do automóvel
         public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Velocidade { get; set; }
+        public int Ano { get; set; }
+        public string Imagen { get; set; }
+
+
+
+        // Criar um construtor para a classe Automovel
         public Automovel() { 
             
         }
+
+        public Automovel(string marca)
+        {
+            this.Marca = marca;
+        }
     }
 }
+
+//    private string Marca;
+
+////public void SetMarca(string marca)
+////{
+////    this.Marca = marca;
+////}
+
+////public string GetMarca()
+////{
+////    return this.Marca;
+////}
